d7f223a475026d7c45073195cebd45e38a359877
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2017-03-29 15:26:45 +0200

    added revision_created_per_weekday for boxplot

---
+++ sql/revision_created_per_weekday.sql
@@ -0,0 +1,10 @@
+select DATE_FORMAT(d.`date`, "%a") as `Day`, count(r.createdtime) as `Created`, DATE_FORMAT(d.`date`, "%u") as `weeknumber`, DATE_FORMAT(d.`date`, "%w") as `day_of_week`
+from
+dates as d
+LEFT join
+(
+SELECT DATE(rev_createdTime) as createdtime FROM gm_libreoffice.t_revision order by DATE(rev_createdTime)
+) as r
+on d.date = createdtime
+group by DATE_FORMAT(d.`date`, "%u"), DATE_FORMAT(d.`date`, "%w")
+order by r.createdtime
